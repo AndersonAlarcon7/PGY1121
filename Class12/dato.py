f0112718fe2764adf77cf287d2eb4ce4abeefa05
AndersonAlarcon7 <134542054+AndersonAlarcon7@users.noreply.github.com>
2023-05-24 14:34:02 -0400

    Archivo Datos.py Sin Validar

---
+++ Class12/dato.py
@@ -0,0 +1,11 @@
+# Ejemplo de uso de repositorio
+ 
+print("Datos Personales")
+print("==========================\n")
+Vnom = input("Ingrese su nombre: ")
+Vedad =int(input("Ingrese su edad :"))
+print("=========================5")
+print(f"Su nombre es:{Vnom}")
+print(f"Su edad es:{Vedad}")
+
+print("Programa finalzado...")
